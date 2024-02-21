aff8d3a8d6af740b28c54d720422e8695c9d46ad
smk762 <35845239+smk762@users.noreply.github.com>
2024-02-21 17:20:12 +0800

    Update update_coins.sh
    closes https://github.com/KomodoPlatform/mm2-client/issues/12

--- update_coins.sh
+++ update_coins.sh
@@ -1,2 +1,22 @@
-rm coins_config.json
-wget https://raw.githubusercontent.com/KomodoPlatform/coins/master/utils/coins_config.json
+#!/bin/bash
+
+# Use this as a daily cron job
+
+cd /home/admin/mm2-client/mm2_tools_server
+# backup existing coins_config.json
+cp /home/admin/mm2-client/coins_config.json /home/admin/mm2-client/coins_config.json.bk
+# Get new coins_config.json from source
+/usr/bin/curl https://raw.githubusercontent.com/KomodoPlatform/coins/master/utils/coins_config.json -o /home/admin/mm2-client/coins_config.json
+# Check new coins_config.json file is valid
+data=$(cat /home/admin/mm2-client/coins_config.json | wc -l)
+# Revert to old file if new file is invalid
+if [[ "$data" -eq 1 ]]; then
+    /home/admin/mm2-client/coins_config.json.bk /home/admin/mm2-client/coins_config.json
+fi
+# Rebuild service
+/usr/local/go/bin/go build -o /home/admin/mm2-client/prices_komodo_earth /home/admin/mm2-client/cmd/mm2_tools_server/mm2_tools_server.go
+# restart service
+/bin/systemctl restart prices-komodo-earth
+# Print date for logs
+now=$(date +%m-%d-%Y)
+echo "prices updated at ${now}"
