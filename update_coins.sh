05fcd9f6860a3eebe34bdc45c3334bde473e607f
smk762 <smk@komodoplatform.com>
2024-12-21 15:29:30 +0100

    improve coins update script

--- update_coins.sh
+++ update_coins.sh
@@ -1,2 +1,7 @@
-rm coins_config.json
-wget https://raw.githubusercontent.com/KomodoPlatform/coins/master/utils/coins_config.json
+#!/bin/bash
+
+FOLDER="/home/admin/mm2-client"
+rm ${FOLDER}/coins_config.json
+rm ${FOLDER}/coins
+wget -P ${FOLDER} https://raw.githubusercontent.com/KomodoPlatform/coins/master/utils/coins_config.json
+wget -P ${FOLDER} https://raw.githubusercontent.com/KomodoPlatform/coins/master/coins
